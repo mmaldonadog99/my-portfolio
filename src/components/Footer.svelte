8b819b87437f8be88498087cdb53fe793958d27e
mmaldonadog99 <mmaldonadog99@gmail.com>
2023-10-03 01:29:34 -0500

    message

---
+++ src/components/Footer.svelte
@@ -0,0 +1,34 @@
+<footer
+    class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
+>
+    <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+        Connect with me &darr;
+    </p>
+    <div class="flex flex-col gap-4 items-center justify-center">
+        <p><b class="pr-2">Email</b> mmaldonadog99@gmail.com</p>
+        <p>
+            <b class="pr-2">GitHub</b>
+            <a href="https://github.com/mmaldonadog99" target="_blank" class="text-violet-400"
+                >Manuel Maldonado<sup class=""
+                    ><span class="text-xs scale-75 pl-0.5"
+                        ><i
+                            class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                        /></span
+                    ></sup
+                ></a
+            >
+        </p>
+        <p>
+            <b class="pr-2">LinkedIn</b>
+            <a href="https://www.linkedin.com/in/manuel-maldonado-gamarra-058045207/" target="_blank" class="text-violet-400"
+                >Manuel Maldonado<sup class=""
+                    ><span class="text-xs scale-75 pl-0.5"
+                        ><i
+                            class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                        /></span
+                    ></sup
+                ></a
+            >
+        </p>
+    </div>
+</footer>
---
+++ src/components/Main.svelte
@@ -0,0 +1,193 @@
+<script>
+    import Step from "./Step.svelte";
+
+    let steps =[
+        {name: 'Diners Store', icon: 'fa-solid fa-cart-shopping', description:'Diners Store is a marketplace created with Next.Js, Commerce.Js & stripe! Commerce.Js is a CMS for managing projects and Stripe is used for all transaction handling'},
+        {name: 'Personal Portfolio', icon: 'fa-solid fa-handshake'},
+        {name: 'Native iOS app', icon: 'fa-brands fa-apple'},
+    ];
+    let benefits = [
+        {
+            metric: "10x",
+            name: "A self taught developer",
+            description:
+                "I taught myself to code using free online resources and absolutely fell in love with the creativity and problem solving that is involved in developing and engineering innovative new online experiences. Starting of with JavaScript, HTML & CSS and evolving my knowledge base to include JavaScript frameworks, backend programming, design, cloud services and much much more.",
+        },
+        {
+            name: "A Marketing professional",
+            description:
+                "Being close to finishing my Bachellors degree in Marketing, not only do I bring my coding skills to the table, but also the business knowledge required to make a company succeed. They say teamwork makes the dreamwork, so why not combine both?",
+        },
+        {
+            name: "An excellent communicator",
+            description:
+                "Communication is key and it's a paramount value of mine. I believe in transparency and constructive communication above all else. This helps me develop deep relationships and ensures my effectiveness and productivity in any work space with any team.",
+        },
+    ];
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+    <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+        <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+            <h2 class="semi-bold text-4xl sm:text-5xl md:text-6xl">
+                Hi! I'm <span class="poppins text-violet-400">Manuel</span> Maldonado <br/>Full Stack 
+                <span class="poppins text-violet-400">Developer</span>
+            </h2>
+            <p class="text-base sm:text-lg md:text-xl">
+                My <span class="text-violet-400">favorite tech</span> includes JavaScript (React.Js, Next.Js or SvelteKit), TailWindCSS, NodeJs + Express.Js, React Native & Figma!
+            </p>
+            <a
+                class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950" href="https://www.linkedin.com/in/manuel-maldonado-gamarra-058045207/" target="_blank"
+            >
+                <div
+                    class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+                />
+                <h4 class="relative z-9">Get in touch &rarr;</h4>
+        </a>
+
+        </div>
+        <div class="relative shadow-2xl grid place-items-center">
+            <img src={"images/Carnet-bg-modified.png"} alt="Profile image" class="object-cover z-[2] max-h-[70vh]"/> 
+        </div>
+    </section>
+
+
+    <section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
+        <div class="flex flex-col gap-2 text-center">
+            <h6 class="text-lg sm:text-xl md:text-2xl">
+                A few of my creative endeavors.
+            </h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                Curious to <span class="poppins text-violet-400">see</span> my work?
+            </h3>
+        </div>
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            target="_blank"
+            class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200">
+            <i class="fa-regular fa-circle-play" />
+            <p>Watch the video</p>
+        </a>
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
+            <Step step={steps[0]}>
+                <p>
+                    Diners Store is a marketplace created with <strong class="text-violet-400">Next.Js, Commerce.Js, Stripe & Node.Js + Express.Js!</strong> Commerce.Js is a CMS for managing projects and Stripe is used for all transaction handling.
+                </p>
+            </Step>
+            <Step step={steps[1]}>
+                <p>
+                    Since I plan on doing more, and better projects in the future, this portfolio won't last me forever. Created with <strong class="text-violet-400">SvelteKit & TailwindCSS</strong>, this Web App is intended to hold my future work
+                </p>
+            </Step>
+            <Step step={steps[2]}>
+                <p>
+                    This iOS weather app was developed using <strong class="text-violet-400">OpenWeather API</strong> and <strong class="text-violet-400">React Native</strong>. It displays the weather in over 200,000 cities around the world in real time!
+                </p>
+            </Step>
+        </div>
+    </section>
+    <section id="about" class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative">
+        <div class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4">
+            <h6 class="text-lg sm:text-xl md:text-2xl">Want to know more?</h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                A bit <span class="poppins text-violet-400">about</span> me.
+            </h3>
+        </div>
+        <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">
+            I am . . .
+        </p>
+        <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+            {#each benefits as benefit, index}  
+            <div class="flex gap-6 sm:gap-8">
+                <p
+                    class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold"
+                >
+                    0{index + 1}
+                </p>
+                <div class="flex flex-col gap-6 sm:gap-8">
+                    <h3 class="text-2xl sm:text-3xl md:text-5xl">
+                        {benefit.name}
+                    </h3>
+                    <p>{benefit.description}</p>
+                </div>
+            </div>
+        {/each}
+    </div>
+    <h5 class={" text-2xl sm:text-3xl font-semibold text-center poppins "}>
+        The <span class="text-violet-400">Complete</span> Package
+    </h5>
+    <div
+        class="flex flex-col overflow-x-scroll gap-10 max-w-[800px] mx-auto w-full"
+    >
+        <table class="bg-white text-slate-700 rounded text-center">
+            <thead class={"border-b border-solid border-slate-200  "}>
+                <tr class="">
+                    <th />
+                    <th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
+                    <th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
+                    <th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
+                    <th
+                        class="bg-violet-700 text-white whitespace-nowrap p-4 px-8"
+                        >Me</th
+                    >
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="border-b border-solid border-slate-200">
+                    <td
+                        class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+                        >Dedication</td
+                    >
+                    <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                    <td><i class="fa-solid fa-check text-slate-500" /></td>
+                    <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                    <td><i class="fa-solid fa-check text-green-500" /></td>
+                </tr>
+                <tr class="border-t border-solid border-slate-200">
+                    <td
+                        class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+                        >Marketing Skills</td
+                    >
+                    <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                    <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                    <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                    <td><i class="fa-solid fa-check text-green-500" /></td>
+                </tr>
+                <tr class="border-b border-solid border-slate-200">
+                    <td
+                        class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+                        >Critical Thought</td
+                    >
+                    <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                    <td><i class="fa-solid fa-check text-slate-500" /></td>
+                    <td><i class="fa-solid fa-check text-slate-500" /></td>
+                    <td><i class="fa-solid fa-check text-green-500" /></td>
+                </tr>
+                <tr>
+                    <td
+                        class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+                        >Interpersonal Skills</td
+                    >
+                    <td><i class="fa-solid fa-check text-slate-500" /></td>
+                    <td><i class="fa-solid fa-check text-slate-500" /></td>
+                    <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                    <td><i class="fa-solid fa-check text-green-500" /></td>
+                </tr>
+                <tr class="border-t border-solid border-slate-200">
+                    <td
+                        class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+                        >Progamming Ability</td
+                    >
+                    <td><i class="fa-solid fa-check text-slate-500" /></td>
+                    <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                    <td><i class="fa-solid fa-check text-slate-500" /></td>
+                    <td><i class="fa-solid fa-check text-green-500" /></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="mx-auto -mt-12 italic opacity-50">
+        <p>Scroll to see more &rarr; </p>
+    </div>
+    <p class="mx-auto font-semibold text-xl sm:text-xl md:text-4xl">So why not <strong class="text-violet-400">invest?</strong></p>
+</section>
+</main>
